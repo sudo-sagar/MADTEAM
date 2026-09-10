1a70f74377b554dcb0a49a1aba1756093b6abadd
sudo-sagar <jadhavandsagar@gmail.com>
2026-09-11 01:07:30 +0530

    pm_agent

--- src/agents/pm_agent.py
+++ src/agents/pm_agent.py
@@ -1,11 +1,13 @@
-from langchain_openai import ChatOpenAI
+#from langchain_openai import ChatOpenAI
+from langchain_community.chat_models import ChatOllama
 from langchain_core.messages import SystemMessage, HumanMessage
 
 def pm_agent(state: dict) -> dict:
     """Project Manager: Breaks requirements into a plan"""
     print("\n🧠 [PM] Analyzing requirements...")
     
-    llm = ChatOpenAI(model="gpt-4o-mini", temperature=0.3)
+    #llm = ChatOpenAI(model="gpt-4o-mini", temperature=0.3)
+    llm = ChatOllama(model="llama3.2:3b", temperature=0)
     
     messages = [
         SystemMessage(content="""You are a Technical Project Manager. 
